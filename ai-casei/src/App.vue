27a985622cedbcd5638173dd461656f908215ca0
thulio31 <163910873+thulio31@users.noreply.github.com>
2024-11-11 15:14:12 -0300

    first commit

---
+++ ai-casei/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <NavBar />
+    <RouterView />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from './components/Footer.vue';
+import NavBar from './components/NavBar.vue';
+
+export default{
+  name: 'App',
+  components:{
+    Footer,
+    NavBar
+  }  
+}
+</script>
---
+++ ai-casei/src/components/NavBar.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <div class="main-menu">
+            <div class="logo">
+                <RouterLink>
+                    <img src="" alt="">
+                </RouterLink>
+            </div>
+            <div class="menu">
+                <header> 
+                    <nav id="nav-1">
+                        <ul>
+                            <RouterLink><li>Meu casamento</li></RouterLink>
+                            <RouterLink><li>Espaços</li></RouterLink>
+                            <RouterLink><li>Noivos</li></RouterLink>     
+                            <RouterLink><li>Serviços</li></RouterLink>                                                   
+                            <RouterLink><li>Lua de mel</li></RouterLink>
+                        </ul>
+                    </nav>
+                </header>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'NavBar'
+}
+</script>
+
+<style scoped>
+
+</style>
