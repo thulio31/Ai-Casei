38e1620069f102679b338e8e31ca2207281d959d
thulio31 <163910873+thulio31@users.noreply.github.com>
2024-11-12 17:11:00 -0300

    gg

--- ai-casei/src/components/Footer.vue
+++ ai-casei/src/components/Footer.vue
@@ -1,6 +1,39 @@
 <template>
     <div>
-
+        <div class="footer-main">
+            <div class="links">
+                <div class="lisks-content">
+                    <RouterLink><li>Espaços</li></RouterLink>
+                    <RouterLink><li>Sobre Nos</li></RouterLink>
+                    <RouterLink><li>Noivos</li></RouterLink>
+                    <RouterLink><li>Casamento</li></RouterLink>
+                </div>
+            </div>
+            <div class="content">
+                <div class="content-slogan">                    
+                    <RouterLink to="/">
+                        <li><i class="fa-solid fa-heart" style="color: #df2626;"></i>Ai Casei!</li>
+                    </RouterLink>
+                    Planeje, compartilhe e eternize o seu grande dia em um só lugar. O começo da sua história merece o cenário perfeito.
+                </div>
+            </div>
+            <div class="social">
+                <div class="social-content">
+                    <li>
+                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa-brands fa-x-twitter"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fab fa-youtube"></i></a>
+                    </li>
+                </div>
+            </div>            
+        </div>
     </div>
 </template>
 
--- ai-casei/src/components/NavBar.vue
+++ ai-casei/src/components/NavBar.vue
@@ -2,15 +2,15 @@
     <div>
         <div class="main-menu">
             <div class="logo">
-                <RouterLink>
-                    <li id="li-logo"><i class="fa-solid fa-heart" style="color: #df2626;"></i>Ai Casei!</li>
+                <RouterLink to="/">
+                    <li><i class="fa-solid fa-heart" style="color: #df2626;"></i>Ai Casei!</li>
                 </RouterLink>
             </div>
             <div class="menu">
                 <header> 
                     <nav id="nav">
                         <ul>
-                            <RouterLink><li>Meu casamento</li></RouterLink>
+                             <RouterLink><li>Meu casamento</li></RouterLink><!--colocar os diretorios -->
                             <RouterLink><li>Espaços</li></RouterLink>
                             <RouterLink><li>Noivos</li></RouterLink>     
                             <RouterLink><li>Serviços</li></RouterLink>                                                   
@@ -30,26 +30,4 @@ export default{
 </script>
 
 <style scoped>
-.main-menu {
-    display:  flex;
-}
-.main-menu a{
-    text-decoration: none;
-}
-.main-menu .logo li{
-    list-style: none;
-    font-weight: bold;
-    color: #f8f8f8;
-    text-shadow: 0px -1px 2px #aaaa;    
-    font-size: 1.6rem;
-    margin-top: 0.9rem ;
-}
-
-.main-menu .logo li:hover{    
-    color: #eca39f;
-    transition: 0.5s;
-    text-shadow: 0px -1px 2px #464444;
-}
-
-
 </style>
